7fa00747e69f14b32fb365f1b06c60e70733ddb2
eugenekkkhan <eugenekhananov@yandex.ru>
2025-08-03 03:14:09 +0300

    Add new pages for ConnectionProblems, Faq, Features, and Videos; update RouterComponent to include new routes

--- src/RouterComponent.tsx
+++ src/RouterComponent.tsx
@@ -3,10 +3,11 @@ import App from "./App";
 import AdminPage from "./AdminPage";
 import AdminAuth from "./AdminAuth";
 import { getCookie } from "./utils";
-import ConnectionProblems from "./pages/ConnectionProblems";
-import Faq from "./pages/Faq";
-import Features from "./pages/Features";
+import ConnectionProblems from "./pages/ConnectionProblemsPage";
+import Faq from "./pages/FaqPage";
+import Features from "./pages/FeaturesPage";
 import useCustomTheme from "./customHooks/useCustomTheme";
+import Videos from "./pages/VideosPage";
 
 const RouterComponent = () => {
   const isAuthenticated = getCookie("token");
@@ -22,6 +23,7 @@ const RouterComponent = () => {
         <Route path="/connection-problems" element={<ConnectionProblems />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/features" element={<Features />} />
+        <Route path="/videos" element={<Videos/ >} />
       </Routes>
     </BrowserRouter>
   );
